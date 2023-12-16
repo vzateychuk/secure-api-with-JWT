facb6e20ba7086cac7b2e85b326f04e451d720be
vzateychuk <vzateychuk@gmail.com>
2023-12-16 17:54:08 +0300

    secured home endpoint

--- src/main/java/vez/practice/jwtdemo/StartApplication.java
+++ src/main/java/vez/practice/jwtdemo/StartApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecureApiJwtOauth0Application {
+public class StartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecureApiJwtOauth0Application.class, args);
+		SpringApplication.run(StartApplication.class, args);
 	}
 
 }
